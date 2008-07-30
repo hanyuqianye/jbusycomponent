bf30a63dcbebe227254088b41c9e4fa897de2e0c
sebastien_288 <sebastien_288@yahoo.fr>
2008-07-30 06:03:39 +0000

    Mark all files with LGPG licence

--- JBusyComponent/src/org/divxdede/swing/busy/BusyModel.java
+++ JBusyComponent/src/org/divxdede/swing/busy/BusyModel.java
@@ -1,3 +1,22 @@
+/*
+ * 
+ * Copyright (c) 2007 ANDRE Sébastien (divxdede).  All rights reserved.
+ * BusyModel.java is a part of this JBusyComponent library
+ * ====================================================================
+ * 
+ * JBusyComponent library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or any later version.
+ * 
+ * This is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package org.divxdede.swing.busy;
 
 import javax.swing.BoundedRangeModel;
